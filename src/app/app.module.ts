0519fedfb9e604d4cba5b21059988071238b1f8f
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2022-07-23 18:26:53 +0600

    vorm val name mail password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormValComponent } from './form-val/form-val.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormValComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
+  // imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
